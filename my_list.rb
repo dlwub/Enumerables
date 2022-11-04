c27691a64885e994d731b17027bd697f9acae5d7
benjamin kisenge <benkisenge03@gmail.com>
2022-11-04 20:16:32 +0200

    Solve linter error

--- my_list.rb
+++ my_list.rb
@@ -12,8 +12,8 @@ class MyList
 end
 
 list = my_list.new(1, 2, 3, 4)
-puts (list.all? { |e| e < 5 })
-puts (list.all? { |e| e > 5 })
-puts (list.any? { |e| e == 2 })
-puts (list.any? { |e| e == 5 })
-puts (list.filter? { |e| e.&:even? })
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter? { |e| e.&:even? })
