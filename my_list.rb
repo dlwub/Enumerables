926ca211c2cf9bc7f6f813a8bf7d8e21f942ae45
benjamin kisenge <benkisenge03@gmail.com>
2022-11-04 20:20:41 +0200

    Fix error

--- my_list.rb
+++ my_list.rb
@@ -11,9 +11,9 @@ class MyList
   end
 end
 
-list = my_list.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
 puts(list.any? { |e| e == 2 })
 puts(list.any? { |e| e == 5 })
-puts(list.filter? { |e| e.&:even? })
+puts list.filter?(&:even?)
